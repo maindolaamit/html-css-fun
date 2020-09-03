9a608b6b889e1ae3d9568109011c02563fb25e75
maindolaamit <maindola.amit@gmail.com>
2020-09-03 19:07:28 +0530

    Search working

--- tab-forms/Code.js
+++ tab-forms/Code.js
@@ -1,3 +1,19 @@
+//# Main Function
+function doGet(request) {
+    Logger.log("Main function");
+    var template = HtmlService.createTemplateFromFile('MainPage');
+    //  Logger.log(template.getCode());
+    return template.evaluate();
+}
+
+//# Function to include CSS file
+function include(filename) {
+    Logger.log("Including file : " + filename);
+    var output = HtmlService.createHtmlOutputFromFile(filename);
+    return output.getContent();
+}
+
+// ###############################################################################################################################################
 var salesTrxSheetURL = "https://docs.google.com/spreadsheets/d/1P_3QPMPP6FxSQAGKyG-F5ISzjO_wU_N1_6I6liD7WyA/edit#gid=0";
 var purchaseTrxSheetURL = "https://docs.google.com/spreadsheets/d/17igkqIv1TEYPBhFbBlzg3zJA8ZOZHwWFY77HtdCW7Ys/edit#gid=651579054";
 var custSetupSheetURL = "https://docs.google.com/spreadsheets/d/1SuXkpfhFDJzCdj55t9xvtzkpPHKQmc1DROyiTgzv8rE/edit#gid=0";
@@ -13,25 +29,81 @@ var invNamingLogicSheetName = "INV_NAMING_CONVENTION";
 var currentDate = new Date();
 var invNumLength = 5;
 
-//# Main Function
-//function doGet(e) {
-//  return HtmlService
-//    .createHtmlOutputFromFile('Page').e
-//    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-//}
-function doGet(request) {
-    Logger.log("Main function");
-    var template = HtmlService.createTemplateFromFile('MainPage');
-    //  Logger.log(template.getCode());
-    return template.evaluate();
+// Search Customer
+function searchCustomer(custSearchInfo) {
+    Logger.log(custSearchInfo);
+    const ss = SpreadsheetApp.openByUrl(custSetupSheetURL).getSheetByName(custSetupSheetName);
+    var lastRow = ss.getLastRow();
+    var lastColumn = ss.getLastColumn();
+    let srchCol = "", srchValue = "";
+
+    // Check if customer id is given
+    if (custSearchInfo.id) {
+        srchCol = 1;
+        srchValue = custSearchInfo.id
+    }
+    else if (custSearchInfo.phone) {
+        srchCol = 4;
+        srchValue = custSearchInfo.phone;
+    }
+    else if (custSearchInfo.email) {
+        srchCol = 6;
+        srchValue = custSearchInfo.email;
+    }
+    // Loop and search for the details
+    var srchData = ss.getRange(2, srchCol, lastRow-1).getValues();
+    for (var i = 0; i < srchData.length; i++) {
+        Logger.log(srchData[i].toString());
+        if (srchData[i].toString() === srchValue) {
+            let data = ss.getRange(i+2, 1, 1, lastColumn).getValues()[0]
+            return {status:"SUCCESS", custData:data};         
+        }
+    }
+    return {status:"NOT_FOUND", data:null};
 }
 
-//# Function to include CSS file
-function include(filename) {
-    Logger.log("Including file : " + filename);
-    var output = HtmlService.createHtmlOutputFromFile(filename);
-    return output.getContent();
+function saveCustomer(customerInfo) {
+    Logger.log(customerInfo);
 }
+
+// Search Supplier
+function searchSupplier(suppSearchInfo) {
+    Logger.log(suppSearchInfo);
+    const ss = SpreadsheetApp.openByUrl(custSetupSheetURL).getSheetByName(custSetupSheetName);
+    var lastRow = ss.getLastRow();
+    var lastColumn = ss.getLastColumn();
+    let srchCol = "", srchValue = "";
+
+    // Check if customer id is given
+    if (suppSearchInfo.id) {
+        srchCol = 1;
+        srchValue = suppSearchInfo.id
+    }
+    else if (suppSearchInfo.phone) {
+        srchCol = 4;
+        srchValue = suppSearchInfo.phone;
+    }
+    else if (suppSearchInfo.email) {
+        srchCol = 6;
+        srchValue = suppSearchInfo.email;
+    }
+    else if (suppSearchInfo.bankAccount) {
+        srchCol = 8;
+        srchValue = suppSearchInfo.bankAccount;
+    }
+    // Loop and search for the details
+    var srchData = ss.getRange(2, srchCol, lastRow-1).getValues();
+    for (var i = 0; i < srchData.length; i++) {
+        Logger.log(srchData[i].toString());
+        if (srchData[i].toString() === srchValue) {
+            let data = ss.getRange(i+2, 1, 1, lastColumn).getValues()[0]
+            return {status:"SUCCESS", suppData:data};         
+        }
+    }
+    return {status:"NOT_FOUND", data:null};
+}
+
+
 
 //# Function to Save sales Transaction
 function saveSalesTrx(formData) {
@@ -47,36 +119,24 @@ function saveSalesTrx(formData) {
 // Function to get new Sales Invoice Number
 function getSalesInvNum() {
     Logger.log("Autogenerating sales invoice number");
-    var sheet = SpreadsheetApp.openByUrl(invNamingLogicSheetURL).getSheetByName(invNamingLogicSheetName);
-    // This represents ALL the data
-    var range = sheet.getDataRange();
-    var values = range.getValues();
-    Logger.log(values);
     // Get the prefix for Invoice
-    let prefix = "";
-    for (var i = 0; i < values.length; i++) {
-        if (values[i][0] == "SALES_INVNUM") {
-            prefix = values[i][1]
-            break;
-        }
-    }
+    let prefix = "CEK/" + currentDate.getFullYear();
     // Fetch the max non empty row
     var salesSheet = SpreadsheetApp.openByUrl(salesTrxSheetURL).getSheetByName(salesTrxSheetName);
     let lastRowNum = salesSheet.getLastRow();
-    let newRowNum = padZeroToNumber(lastRowNum, invNumLength); // new Array(invNumLength - (lastRowNum.toString().length) + 1).join('0') + (lastRowNum+1);  
-    let invNum = prefix + currentDate.getFullYear() + padZeroToNumber(currentDate.getMonth(), 2) + "-" + newRowNum;
+    let newRowNum = padZeroToNumber(lastRowNum + 1, invNumLength); // new Array(invNumLength - (lastRowNum.toString().length) + 1).join('0') + (lastRowNum+1);  
+    let invNum = prefix + "/" + newRowNum;
 
     Logger.log(invNum);
     return invNum;
 }
 
-// Add 0 to the number
-function padZeroToNumber() {
-    let num = 1000;
-    let desiredWidth = 3;
-    Logger.log(num.toString().length);
-    Logger.log(desiredWidth - (num.toString().length));
-    let value = new Array(desiredWidth - (num.toString().length) + 1).join('0') + num;
-    Logger.log(value)
-    return value;
+// Pad zero to the input number making it upto desiredWidth
+function padZeroToNumber(num, desiredWidth) {
+    if (num.toString().length >= desiredWidth) {
+        return num;
+    } else {
+        let value = new Array(desiredWidth - (num.toString().length) + 1).join('0') + num;
+        return value;
+    }
 }
